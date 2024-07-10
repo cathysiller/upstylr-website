6d9e7d9afb78557cb398bdd38de9129449826b56
Catherine Siller <cathysiller503@gmail.com>
2024-07-09 20:29:01 -0500

    first commit for coming soon page

---
+++ src/app/fonts.js
@@ -0,0 +1,11 @@
+import { Playfair_Display, Raleway } from "next/font/google";
+
+export const playfair_display = Playfair_Display({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const raleway = Raleway({
+  subsets: ["latin"],
+  display: "swap",
+});
